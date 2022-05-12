15f3d68ca1a2e3acd52af803abff926cf34207ab
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-12 17:44:07 -0300

    rota de criação de usuário

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
+const usuariosController = require("../controllers/usuariosController");
+const authController = require("../controllers/authController");
+const validarCriacaoUsuario = require("../validations/usuarios/create");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Registro de novo usuário
+router.post("/new", usuariosController.registro);
+
+// Login de usuário
+// router.post("/login", authController.login);
 
 module.exports = router;
